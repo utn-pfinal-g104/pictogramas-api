d8257188bc8afec2bd79c179ddb68cb9a7f88ae1
Leonardo Orlando <26606912+orlandoleonardo@users.noreply.github.com>
2022-09-06 20:32:08 -0300

    Update Script creacion pictogramasdblocal.sql

--- docs/Script creacion pictogramasdblocal.sql
+++ docs/Script creacion pictogramasdblocal.sql
@@ -192,12 +192,25 @@ CREATE TABLE [dbo].[Usuarios](
 ) ON [PRIMARY]
 GO
 
-Create table FavoritosPorUsuarios(
-Id int PRIMARY KEY,
-UsuarioId int FOREIGN KEY REFERENCES Usuarios(Id),
-PictogramaId int foreign key references Pictogramas(Id)
-)
+CREATE TABLE [dbo].[FavoritosPorUsuarios](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UsuarioId] [int] NULL,
+	[PictogramaId] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
+
+ALTER TABLE [dbo].[FavoritosPorUsuarios]  WITH CHECK ADD FOREIGN KEY([PictogramaId])
+REFERENCES [dbo].[Pictogramas] ([Id])
+GO
+
+ALTER TABLE [dbo].[FavoritosPorUsuarios]  WITH CHECK ADD FOREIGN KEY([UsuarioId])
+REFERENCES [dbo].[Usuarios] ([Id])
+GO
+
 
 ALTER TABLE [dbo].[Categorias] ADD  DEFAULT ((0)) FOR [Nivel]
 GO
