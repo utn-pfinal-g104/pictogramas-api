8db16fda4b7b25709785a5887b634ed90f2c68d7
RHV044 <s_rhv@hotmail.com>
2022-10-11 08:01:56 -0300

    DB script updated with Pizarras table creation

--- docs/Script creacion pictogramasdblocal.sql
+++ docs/Script creacion pictogramasdblocal.sql
@@ -3,10 +3,10 @@ GO
 /****** Object:  Database [pictogramasdblocal]    Script Date: 22/8/2022 20:57:16 ******/
 CREATE DATABASE [pictogramasdblocal]
  CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'pictogramasdblocal', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\pictogramasdblocal.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'pictogramasdblocal_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\pictogramasdblocal_log.ldf' , SIZE = 73728KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ --ON  PRIMARY 
+--( NAME = N'pictogramasdblocal', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\pictogramasdblocal.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+-- LOG ON 
+--( NAME = N'pictogramasdblocal_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\pictogramasdblocal_log.ldf' , SIZE = 73728KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
  WITH CATALOG_COLLATION = DATABASE_DEFAULT
 GO
 ALTER DATABASE [pictogramasdblocal] SET COMPATIBILITY_LEVEL = 150
@@ -218,6 +218,21 @@ GO
 
 ALTER TABLE [dbo].[FavoritosPorUsuarios]  WITH CHECK ADD FOREIGN KEY([UsuarioId])
 REFERENCES [dbo].[Usuarios] ([Id])
+GO
+
+CREATE TABLE [dbo].[Pizarras](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UsuarioId] [int] NOT NULL,
+	[Filas] [int] NOT NULL,
+	[Columnas] [int] NOT NULL,
+	[Nombre] [varchar](MAX) NOT NULL,
+	[UltimaActualizacion] datetime2 NOT NULL,
+
+ CONSTRAINT [PK_Pizarras] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
 ALTER TABLE [dbo].[Pizarras]  WITH CHECK ADD  CONSTRAINT [FK_Pizarras_Usuarios] FOREIGN KEY([UsuarioId])
