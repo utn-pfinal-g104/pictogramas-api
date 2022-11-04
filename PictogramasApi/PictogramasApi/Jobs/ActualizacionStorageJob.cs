7c656a65b51a3b1075b3206be23899765ac70a0d
Gonzalo Vaquero <gonzovaq@gmail.com>
2022-11-03 21:22:27 -0300

    asincronismo para el guardado de pictogramas

--- PictogramasApi/PictogramasApi/Jobs/ActualizacionStorageJob.cs
+++ PictogramasApi/PictogramasApi/Jobs/ActualizacionStorageJob.cs
@@ -91,13 +91,12 @@ namespace PictogramasApi.Jobs
             List<Stream> pictogramasAsStreams = new List<Stream>();
             try
             {
-                foreach (var pictograma in pictogramasArasaac)
+                Parallel.ForEach(pictogramasArasaac, async (pictograma) =>
                 {
-                    //TODO: Aca podriamos tirar grupos de tasks que corran en simultaneo para acelerar el proceso
                     var pictogramaAsStream = await _arasaacService.ObtenerPictogramaDeArasaac(pictograma._id);
                     _storageMgmt.Guardar(pictogramaAsStream, $"{pictogramasNuestros.FirstOrDefault(p => p.IdArasaac == pictograma._id).Id}"); // TODO: Con que nombre lo guardamos? por ahora lo estamos guardando con el id
                     Console.WriteLine($"Se inserto pictograma: {pictograma._id}");
-                }
+                });
             }
             catch (Exception e)
             {
