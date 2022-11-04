3f287a65c8d40cd7352f18226e3594d7de9f832a
Gonzalo Vaquero <gonzovaq@gmail.com>
2022-11-03 21:04:31 -0300

    agrego healthcheck

---
+++ PictogramasApi/PictogramasApi/Modules/HealthCheckModule.cs
@@ -0,0 +1,16 @@
+﻿using Carter;
+using Carter.Response;
+
+namespace PictogramasApi.Modules
+{
+    public class HealthCheckModule : CarterModule
+    {
+        public HealthCheckModule()
+        {
+            Get("/", async (ctx) =>
+            {
+                await ctx.Response.Negotiate("Healthy");
+            });
+        }
+    }
+}
